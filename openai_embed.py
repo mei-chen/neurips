e9464f5f249b328317be44b1affc6f1a399cb5a9
Mei Chen <meichen.mlc@gmail.com>
2023-12-13 09:31:25 -0600

    m

--- openai_embed.py
+++ openai_embed.py
@@ -16,7 +16,8 @@ df = pd.read_csv('combined.csv')
 df = df.rename(columns={'h5_strong_texts': 'title', 
                         'abstract_texts': 'abstract'})
 df['abstract'] = df['abstract'].str[1:]
-df['combined'] = "Title: " + df['title'] + "\nAbstract: " + df['abstract']
+# df['combined'] = "Title: " + df['title'] + "\nAbstract: " + df['abstract']
+df['combined'] = df['title'] + "\n" + df['abstract']
 print(df['combined'][0])
 
 # df['combined'] = "Title: " + df[0] + " Abstract:" + df[1][2:]
@@ -35,5 +36,5 @@ def get_embedding(text, model="text-embedding-ada-002"):
    return client.embeddings.create(input = [text], model=model).data[0].embedding
 
 df['ada_embedding'] = df['combined'].apply(lambda x: get_embedding(x, model='text-embedding-ada-002'))
-df.to_csv('embedded_combined.csv', index=False)
+df.to_csv('embedded_combined1.csv', index=False)
 
